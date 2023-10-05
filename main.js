c914f25f52cc4eb71d6c1caee5a6fb35ae40d0a3
violetavillamizara <villamizaravioleta@gmail.com>
2023-10-04 22:31:18 -0500

    feat: :sparkles: Api queries
    IMG

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-import { getImgMisions } from "./v3.js";
+import { getImgLaunches, getImgRockets, getImgShips } from "./v3.js";
 
-let launches= await getImgMisions();
+let launches= await getImgLaunches();
+let rockets=await getImgRockets();
+let ships=await getImgShips();
+
+console.log(launches);
+console.log(rockets);
+console.log(ships);
--- v3.js
+++ v3.js
@@ -1,4 +1,4 @@
-export const getImgMisions=async()=>{
+export const getImgLaunches=async()=>{
     let res=await(await fetch("https://api.spacexdata.com/v3/launches")).json();
     let img=res.map(element => {
         let { mission_patch: img, mission_patch_small: img_small } = element.links;
@@ -6,3 +6,21 @@ export const getImgMisions=async()=>{
       });
       return img;
 }
+
+export const getImgRockets=async()=>{
+  let res=await(await fetch("https://api.spacexdata.com/v3/rockets")).json();
+  let img=res.map(e=>{
+    let {flickr_images} = e;
+    return flickr_images;
+  })
+  return img;
+}
+
+export const getImgShips = async () => {
+  let res = await(await fetch("https://api.spacexdata.com/v3/ships")).json();
+  let img = res.map(element =>{
+    let {image} = element;
+    return image;
+  });
+  return img;
+};
